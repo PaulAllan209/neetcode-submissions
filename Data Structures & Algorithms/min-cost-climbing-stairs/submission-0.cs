f44472b0663e4f24697aaa1faaea3a291eafd673
Paul_Allan_Dela_Viña <dpaulallan14@gmail.com>
2026-05-02 09:00:19 +0800

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MinCostClimbingStairs(int[] cost) {
+        int a = cost[0];
+        int b = cost [1];
+
+        for (int i = 2; i < cost.Length; i++) {
+            int temp = cost[i] + Math.Min(a, b);
+            a = b;
+            b = temp;
+        }
+
+        return Math.Min(a, b);
+    }
+}
